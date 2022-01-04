e9c22ba005fb3b4ac926c8308e5514773cd82ac8
kobryl <59873325+kobryl@users.noreply.github.com>
2022-01-04 16:55:32 +0100

    br size

--- image.py
+++ image.py
@@ -13,7 +13,7 @@ def locateCorners():
         tl = pyautogui.locateOnScreen('images/topleft.png')
         br = pyautogui.locateOnScreen('images/bottomright.png')
         begin = tl[0]+4, tl[1]+1
-        end = br[0]+12, br[1]+12
+        end = br[0]+36, br[1]+37
         return begin, end
     except TypeError:
         return None, None
@@ -22,7 +22,7 @@ def locateCorners():
 def locatePlayer(topLeft, bottomRight):
     try:
         playerBox = None
-        playerRegion = (topLeft[0], bottomRight[1] - 25, bottomRight[0] - topLeft[0], 25)
+        playerRegion = (topLeft[0], bottomRight[1] - 27, bottomRight[0] - topLeft[0] + 2, 30)
         playerWhiteBox = pyautogui.locateOnScreen('images/player.png', region=playerRegion)
         playerRedBox = pyautogui.locateOnScreen('images/player_invincible.png', region=playerRegion)
 
