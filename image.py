ae5cc9c67304561fc0073bf682ee6802f88308f6
kobryl <59873325+kobryl@users.noreply.github.com>
2022-01-04 21:24:57 +0100

    cv2 śmierć

--- image.py
+++ image.py
@@ -22,8 +22,9 @@ def locateCorners():
 def locatePlayer(topLeft, bottomRight):
     try:
         playerBox = None
-        # playerRegion = (8, 633, 961, 30) # 1920:1080 100%
+        #playerRegion = (8, 633, 961+8, 30+633) # 1920:1080 100%
         playerRegion = (topLeft[0], bottomRight[1] - 27, bottomRight[0] - topLeft[0] + 2, 28)
+
         playerWhiteBox = pyautogui.locateOnScreen('images/player.png', region=playerRegion)
         playerRedBox = pyautogui.locateOnScreen('images/player_invincible.png', region=playerRegion)
 
@@ -38,12 +39,17 @@ def locatePlayer(topLeft, bottomRight):
         else:
             return playerBox
         return pyautogui.center(playerBox).x
+
     except TypeError:
         return None
 
 '''
-def locateEnemies():
-
+def locateEnemies(topLeft, bottomRight):
+    enemyRegion = (topLeft[0], topLeft[1], bottomRight[0] - topLeft[0] + 2, bottomRight[1] - topLeft[1] - 20)
+    enemies = pyautogui.locateAllOnScreen('images/enemy.png', region=enemyRegion, grayscale=True, confidence=0.8)
+    pyautogui.screenshot('screen.png', region=enemyRegion)
+    for enemy in enemies:
+        print(enemy)
 
 
 def locateEnemyBullets():
