e322b6a4563f6cb93d02ed414054ae7a5e362808
kobryl <59873325+kobryl@users.noreply.github.com>
2022-01-04 15:03:42 +0100

    locatePlayer

--- image.py
+++ image.py
@@ -15,3 +15,19 @@ def locateCorners():
 
 
 def locatePlayer():
+    playerBox = None
+    playerWhiteBox = pyautogui.locateOnScreen('images/player.png')
+    playerRedBox = pyautogui.locateOnScreen('images/plyer_invincible.png')
+    if playerRedBox is not None:
+        playerBox = playerRedBox
+    elif playerWhiteBox is not None:
+        playerBox = playerWhiteBox
+    else:
+        return playerBox
+    x1 = playerBox['left']
+    y1 = playerBox['top']
+    x2 = x1 + playerBox['width']
+    y2 = y1 + playerBox['height']
+
+    center = (x1 + x2) / 2, (y1 + y2) / 2
+    return center
